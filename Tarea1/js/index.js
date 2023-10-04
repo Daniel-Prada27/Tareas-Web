fb929a65d53d5810958f0576d753c35f6166816b
Daniel <danielspradah0@gmail.com>
2023-10-04 15:14:05 -0500

    Agregar logica para creacion y eliminacion

--- Tarea1/js/index.js
+++ Tarea1/js/index.js
@@ -1,13 +1,37 @@
 const input = document.getElementById('input-usuario');
 const agregarBtn = document.getElementById('boton-agregar');
 const contenedor = document.getElementById('contenedor');
-const borrarBtn = document.getElementById('boton.borrar');
+const borrarBtn = document.getElementById('boton-borrar');
 
-agregarBtn.addEventListener('click', obtenerTexto);
+agregarBtn.addEventListener('click', agregar);
+borrarBtn.addEventListener('click', borrar);
 
-function obtenerTexto() {
-    let texto = input.value;
-    console.log(`Prueba de funcionamiento: ${texto}`);
-    input.value = null;
+function agregar() {
+
+    let newEntry = document.createElement('div');
+    newEntry.textContent = obtenerTexto();
+
+    if (newEntry.textContent == null | newEntry.textContent == '') {
+        return;
+    }
+
+    contenedor.appendChild(newEntry);
+
 }
 
+function obtenerTexto() {
+    let texto = input.value.toLowerCase();
+    input.value = null;
+    return texto;
+}
+
+function borrar() {
+    if (tieneElementos()) {
+        let lastChild = contenedor.lastChild;
+        contenedor.removeChild(lastChild);
+    }
+}
+
+function tieneElementos() {
+    return contenedor.childElementCount != 0;
+}
